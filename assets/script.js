2f6c450bd0366b5e96b79c4e86e3a1b330515521
bfinch3 <billyrfinch@gmail.com>
2023-10-10 20:26:23 -0500

    added to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
 //----GLOBAL VARIABLES HOME PAGE----//
-var artistText=$('#search-artist input')
-var locationText=$('#search-city input')
-
-// this function will get the search input provided and redirect to the search page
-form.on('submit', function(event){ 
+var artistText = $('#search-artist input')
+var locationText = $('#search-city input')
 
 // this function will get the search input provided and redirect to the search page
 $(document).ready(function () {
     //listens for all link clicks
-    $('a').click(function (event) { 
-localStorage.setItem('city', locationText.val());
-localStorage.setItem('artist', artistText.val());
+    $('a').click(function (event) {
+        var location = locationText.val() != undefined ? locationText.val() : '';
+        var artist = artistText.val() != undefined ? artistText.val() : '';
 
-        window.location.href =`search-results.html`
-    });
+        localStorage.setItem('city', location);
+        localStorage.setItem('artist', artist);
+
+        window.location.href = `search-results.html`
+
+    })
 })
-
-});
